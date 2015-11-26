78c1a584fc4f5c59a6d8dc7f15c7543d9a331414
Kevin <unconfigured@null.spigotmc.org>
2015-11-26 16:05:24 +0100

    Tp 1 (jusqu'a la 4eme question)

---
+++ Csharptp/Droid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharptp
+{
+    public abstract class Droid
+    {
+        private bool isInit = false;
+        public virtual void Init() {
+            isInit = true;
+            Console.Write("Coucou");
+            Console.ReadLine();
+        }
+        public bool Work() {
+            if (isInit == false)
+                return false;
+            Console.Write("Coucou");
+            Console.ReadLine();
+            return true;
+                }
+        public void Shutdown()
+        {
+            if (isInit == false)
+                return;
+            Console.ReadLine();
+            Console.Write("Coucou");
+        }
+    }
+}
---
+++ Csharptp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharptp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Droid droid = new C3PO();
+
+            droid.Work();
+            droid.Init();
+            droid.Shutdown();
+
+        }
+    }
+
+    class C3PO : Droid
+    {
+        public override void Init()
+        {
+            base.Init();
+            Console.Write("Coucou");
+        }
+
+
+    }
+}

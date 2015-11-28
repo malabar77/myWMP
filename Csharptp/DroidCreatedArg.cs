58a3fac093fb1921c9984912b28bee662ca9e516
Kevin <kevin.calvez@epitech.eu>
2015-11-28 13:52:02 +0100

    Some changes

---
+++ Csharptp/DroidCreatedArg.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DroidCreatedArg.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Csharptp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class DroidCreatedArg : EventArgs
+    {
+        public DroidCreatedArg(Droid droid)
+        {
+            this.Droid = droid;
+        }
+
+        public Droid Droid { get; private set; }
+    }
+}
--- Csharptp/Tp2/Tp2/MainWindow.xaml.cs
+++ Csharptp/Tp2/Tp2/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Tek3.tp1;
 
 namespace Tp2
 {
     public partial class MainWindow : Window
     {
+        private double TimerDelay;
         public ObservableCollection<Droid> Droids { get; set; }
 
         private DroidFactory usine = new DroidFactory(new List<Type>() {
@@ -29,21 +20,28 @@ namespace Tp2
         {
             InitializeComponent();
             this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            usine.DroidCreated += new EventHandler<DroidCreatedArg>(droidFactory_DroidCreated);
+
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             this.Droids = new ObservableCollection<Droid>();
+            this.LstDroids.ItemsSource = this.Droids;
+            this.TimerDelay = 1.0;
         }
 
-        //private void droidFactory_DroidCreated()
-        //{
-        //    this.Droids.Add();
-        //}
+        private void droidFactory_DroidCreated(object sender, DroidCreatedArg e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                this.Droids.Add(e.Droid);
+            }));
+        }
 
         private void StartUsine(object sender, RoutedEventArgs e)
         {
-            usine.Start(1);
+            usine.Start(this.TimerDelay);
             UsineStatus.Text = "On";
         }
 
@@ -51,6 +49,11 @@ namespace Tp2
         {
             usine.Stop();
             UsineStatus.Text = "Off";
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TimerDelay = slider1.Value;
         }
     }
 }

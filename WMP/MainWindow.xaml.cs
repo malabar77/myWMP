2c2fee502a1da2716aa7a7f314f7e64c84ae3a29
Kevin <kevin.calvez@epitech.eu>
2015-11-29 14:27:02 +0100

    Basic design WMP

---
+++ WMP/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.DirectX.AudioVideoPlayback;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace WMP
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+	}
+
+        private void Lecture_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Parcourir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+	}
+    }
+}

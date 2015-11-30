2d690bf621c63aaed81faddb69ff72e097ec4ad4
Kevin <kevin.calvez@epitech.eu>
2015-11-30 14:36:37 +0100

    Wav file Ok

--- WMP/MainWindow.xaml.cs
+++ WMP/MainWindow.xaml.cs
@@ -15,6 +15,12 @@ using System.Windows.Shapes;
 using Microsoft.DirectX.AudioVideoPlayback;
 using Microsoft.Win32;
 using System.Runtime.InteropServices;
+using System.Media;
+using System.Threading;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
 
 namespace WMP
 {
@@ -23,14 +29,24 @@ namespace WMP
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Audio music;
+        private SoundPlayer player;
+
         public MainWindow()
         {
             InitializeComponent();
-	}
+	    }
 
         private void Lecture_Click(object sender, RoutedEventArgs e)
         {
-
+            //System.Media.SoundPlayer player = new SoundPlayer();
+            //player.SoundLocation = "..\\..\\LARCY.wav";
+            //player.LoadAsync();
+            //player.PlayLooping();
+            //Thread.Sleep(5000);
+            //player.Stop();
+            music = new Audio("..\\..\\Birdy.mp3");
+            music.Play();
         }
 
         private void Pause_Click(object sender, RoutedEventArgs e)
@@ -45,7 +61,7 @@ namespace WMP
 
         private void Parcourir_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)

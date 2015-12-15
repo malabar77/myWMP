513044f4ccc8ad58a53b1da18875a8b3627448f5
Kevin <kevin.calvez@epitech.eu>
2015-12-15 03:23:23 +0100

    Timer refresh and Slider move with timer

--- WMP/MainWindow.xaml.cs
+++ WMP/MainWindow.xaml.cs
@@ -1,58 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.DirectX.AudioVideoPlayback;
 using Microsoft.Win32;
-using System.Runtime.InteropServices;
-using System.Media;
-using System.Threading;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Timers;
+using System.Windows.Threading;
 using System.Windows.Controls.Primitives;
 
 namespace WMP
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         private bool onAir = false;
+        private bool userIsDraggingSlider = false;
+
 
         public MainWindow()
         {
             InitializeComponent();
             media.MediaOpened += new RoutedEventHandler(media_MediaOpened);
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Timer.Text = SetDuration(media.Position.TotalSeconds);
+            sliProgress.Value = media.Position.TotalSeconds;
         }
 
         private void media_MediaOpened(object sender, RoutedEventArgs e)
         {
-//            SetDuration(media.NaturalDuration.TimeSpan.TotalSeconds);
-            SetDuration(media.Position.TotalSeconds);
             sliProgress.Minimum = 0;
             sliProgress.Maximum = media.NaturalDuration.TimeSpan.TotalSeconds;
-            sliProgress.Value = media.Position.TotalSeconds;
 
         }
 
         private void Plalistview_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string path = (string)Playlistviewfull.SelectedItem;
+            string path = (string)Playlistviewfull.Items[Playlistview.SelectedIndex];
             media.Source = new Uri(path);
             media.Play();
         }
@@ -61,13 +48,8 @@ namespace WMP
         {
             DialogFenetre();
         }
-
-        private void timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-
-        }
-
-        private void SetDuration(double duration)
+    
+        private string SetDuration(double duration)
         {
             int n = 1;
             int minutes = 0;
@@ -84,12 +66,11 @@ namespace WMP
                 min = "0" + minutes.ToString();
             if (seconds < 10)
                 sec = "0" + seconds.ToString();
-            Timer.Text = min + ":" + sec;
+            return (min + ":" + sec);
         }
 
         private void Pause_Click(object sender, RoutedEventArgs e)
         {
-            //Timer.Text = mediaPlayer.CurrentPosition.ToString() ;
             onAir = false;
             media.LoadedBehavior = MediaState.Pause;
 
@@ -104,11 +85,11 @@ namespace WMP
         private void DialogFenetre()
         {
             OpenFileDialog fenetre = new OpenFileDialog();
-            //fenetre.Filter = "Fichiers Multimedia|*.wav;*.mp3;*.mp4";
+            fenetre.Filter = "Fichiers Multimedia|*.wav;*.mp3;*.mp4";
             if (fenetre.ShowDialog() == true)
             {
-                Playlistview.Items.Add(fenetre.FileName);
-                Playlistviewfull.Items.Add(fenetre.SafeFileName);
+                Playlistviewfull.Items.Add(fenetre.FileName);
+                Playlistview.Items.Add(fenetre.SafeFileName);
                 this.Title = fenetre.SafeFileName;
                 if (onAir == true)
                 {
@@ -134,8 +115,6 @@ namespace WMP
         {
             media.LoadedBehavior = MediaState.Play;
         }
-        private bool userIsDraggingSlider = false;
-
 
         private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
         {
@@ -150,7 +129,7 @@ namespace WMP
 
         private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            Timer.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+            //Timer.Text = media.Position.TotalSeconds.ToString();
         }
 
     }
